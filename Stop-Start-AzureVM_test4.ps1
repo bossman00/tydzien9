668f74dcddd0f46d2f57b4254d110b9d2544370b
bossman00 <44263459+bossman00@users.noreply.github.com>
2019-04-05 15:31:26 +0200

    Add files via upload

---
+++ Stop-Start-AzureVM_test4.ps1
@@ -0,0 +1,74 @@
+Workflow Stop-Start-AzureVM_test4 
+{ 
+    Param 
+    (    
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
+        [String] 
+        $AzureSubscriptionId, 
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
+        [String] 
+        $AzureVMList="All", 
+        [Parameter(Mandatory=$true)][ValidateSet("Start","Stop")] 
+        [String] 
+        $Action 
+    ) 
+     
+$connectionName = "AzureRunAsConnection"
+try {
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+    "Logging in to Azure..."
+    Connect-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+}
+catch {
+    if (!$servicePrincipalConnection) {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    }
+    else {
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+ 
+    if($AzureVMList -ne "All") 
+    { 
+        $AzureVMs = $AzureVMList.Split(",") 
+        [System.Collections.ArrayList]$AzureVMsToHandle = $AzureVMs 
+    } 
+    else 
+    { 
+        $AzureVMs = (Get-AzureRmVM).Name 
+        [System.Collections.ArrayList]$AzureVMsToHandle = $AzureVMs 
+ 
+    } 
+ 
+    foreach($AzureVM in $AzureVMsToHandle) 
+    { 
+        if(!(Get-AzureRmVM | ? {$_.Name -eq $AzureVM})) 
+        { 
+            throw " AzureVM : [$AzureVM] - Does not exist! - Check your inputs " 
+        } 
+    } 
+ 
+    if($Action -eq "Stop") 
+    { 
+        Write-Output "Stopping VMs"; 
+        foreach -parallel ($AzureVM in $AzureVMsToHandle) 
+        { 
+            Get-AzureRmVM | ? {$_.Name -eq $AzureVM} | Stop-AzureRmVM -Force 
+        } 
+    } 
+    else 
+    { 
+        Write-Output "Starting VMs"; 
+        foreach -parallel ($AzureVM in $AzureVMsToHandle) 
+        { 
+            Get-AzureRmVM | ? {$_.Name -eq $AzureVM} | Start-AzureRmVM 
+        } 
+    } 
+}
